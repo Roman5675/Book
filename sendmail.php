c369d75d84555b67f3eaea8433b46128a2d54cfc
Roman Pecherskikh <pwpec95@gmail.com>
2023-08-14 23:57:40 +0300

    14.08.2023c

--- sendmail.php
+++ sendmail.php
@@ -2,12 +2,12 @@
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
+    require 'PHPMailer/src/Exception.php';
+    require 'PHPMailer/src/PHPMailer.php';
 
     $mail = new PHPMailer(true);
     $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
+    $mail->setLanguage('ru', 'PHPMmailer/language/');
     $mail->IsHtml(true);
 
     //От кого письмо
